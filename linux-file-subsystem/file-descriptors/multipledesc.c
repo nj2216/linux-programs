a6afe1b7ec51925080fcc068dee55960dfc9f26a
Jeevan N <njeevan2216@gmail.com>
2026-07-23 11:36:13 +0530

    Implementation to see multiple descriptors

---
+++ linux-file-subsystem/file-descriptors/multipledesc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int fd1 = open("data.txt", O_RDONLY);
+    int fd2 = open("data.txt", O_RDONLY);
+
+    if (fd1 == -1 || fd2 == -1) { perror("open"); return 1; }
+
+    char buf[16];
+    read(fd1, buf, 10);
+
+    off_t off1 = lseek(fd1, 0, SEEK_CUR);
+    off_t off2 = lseek(fd2, 0, SEEK_CUR);
+
+    printf("fd1 offset: %ld\n", (long) off1);
+    printf("fd2 offset: %ld\n", (long) off2);
+
+    close(fd1);
+    close(fd2);
+    return 0;
+}
